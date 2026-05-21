0f93c1a2583d046871dcb8d7cc65f4a77ee98ecf
diana01022 <diana.bohutska@ametikool.ee>
2026-05-21 15:13:41 +0300

    first commit
    git commit --amend --reset-author#

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+const scrollButtons = document.querySelectorAll(".scroll-btn");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+scrollButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const targetId = button.dataset.target;
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+
+    navLinks.classList.remove("active");
+  });
+});
